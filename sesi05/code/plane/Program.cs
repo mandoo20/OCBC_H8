83fb2b45a735663072c23594ab7a8a4ed1c7938d
mandoo20 <armandofransisco20@gmail.com>
2021-10-13 12:58:45 +0700

    rename folder

---
+++ sesi05/code/plane/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace plane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesawat.class_pesawat pesawat = new Pesawat.class_pesawat();
+            pesawat.nama = "Garuda";
+            pesawat.ketinggian="2500 Kaki";
+
+            pesawat.terbang();
+            pesawat.sudahTerbang();
+            Console.Read();
+        }
+    }
+}
