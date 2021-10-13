1d75a9d30cbe391fb20d1c0623484480328a0c6f
mandoo20 <armandofransisco20@gmail.com>
2021-10-13 08:20:05 +0700

    rev 1 sesi 3

---
+++ sesi4/code/data1.cs
@@ -0,0 +1,20 @@
+using System;
+
+class data1{
+
+    static void Main(){
+
+        int[] aAngka = new int[5];
+        string[] aNama = new string[] {"Joni","Meri","David"};
+        object[] aObjek = {20.33,"Lorem ipsum", DateTime.Now, true,'D'};
+    }
+
+
+    void AllocationMemory(){
+        int x = 4;
+
+        int y = 2;
+
+        Kendaraan mobil = new Kendaraan();
+    }
+}
