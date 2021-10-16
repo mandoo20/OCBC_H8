544811d43008807f74a08c826de18f70366df5d4
mandoo20 <armandofransisco20@gmail.com>
2021-10-17 03:09:14 +0700

    add code sesi 10 with jwt Done Work correctly

---
+++ sesi10/JwtVersion/TodoApp/Configuration/AuthResult.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace TodoApp.Configuration
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public bool Succeess { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ sesi10/JwtVersion/TodoApp/Data/ApiDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TodoApp.Data{
+
+    public class ApiDbContext : IdentityDbContext{
+        public virtual DbSet<ItemData> Items {get; set;}
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+            
+        }
+    }
+
+    
+}
